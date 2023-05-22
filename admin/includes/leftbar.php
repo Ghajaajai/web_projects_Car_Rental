71e746306baf99c8a29f641521e5c0dae4d4272b
152021166 Muhammad Ghaza Azhar Lesmana <114157498+Ghajaajai@users.noreply.github.com>
2023-05-22 20:47:40 +0700

    Initial commit

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,27 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Brands</a>
+<ul>
+<li><a href="create-brand.php">Tambah Brand</a></li>
+<li><a href="manage-brands.php">Setting Brands</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Mobil</a>
+					<ul>
+						<li><a href="post-avehical.php">Tambah Mobil Baru</a></li>
+						<li><a href="manage-vehicles.php">Setting Mobil</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Manage Booking</a></li>
+				<li><a href="manage-contactusquery.php"><i class="fa fa-desktop"></i> Manage Contact Us Query</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Daftar User</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
+
+
+			</ul>
+		</nav>
